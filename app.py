19d8e59f1a3675ab49087c0e552392d38f0d936a
Sigo van der Linde <sigovdlinde@gmail.com>
2023-01-09 04:07:15 +0100

    perfecting stuff

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from dash import Dash, dcc, html, dash_table
-import dash_bootstrap_components as dbc
 from dash.dependencies import Output, Input
 
-from functions import *
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 
 import pandas as pd
 
 import plotly.graph_objects as go
 import plotly.express as px
 
+from functions import *
+
 all_fight_data = open_data('all_fight_data')
+all_f_data = open_data('all_f_data')
+all_r_data = open_data('all_r_data')
 
 all_f_names = all_fighter_names(all_fight_data)
 all_r_names = all_referee_names(all_fight_data)
@@ -25,7 +29,8 @@ group_options = [{'label': s, 'value': s} for s in ["Fighters", "Referees"]]
 chart_options = [{'label': s, 'value': s} for s in ["Line", "Bar"]]
 yesno_options = [{'label': s, 'value': s} for s in ["Yes", "No"]]
 
-structure_f = ["Name", "Date", "KO/TKO", "Submission", "Decision", "Winlose score", "Finish score"]
+structure_f = ["Name", "Date", "Winlose score", "KO/TKO", "Submission", "Decision", "Finish score", "Wins", "Losses", "Draws/NCs",
+			   "Knockdowns", "Takedowns Landed", "Takedowns Attempted", "Reversals", "Submission Attempted", "Control Time"]
 structure_r = ["Name", "Date", "KO/TKO", "Submission", "Decision", "Finish score"]
 
 structure_t = ["KO/TKO", "Submission", "Decision", "Finish score"]
@@ -33,15 +38,13 @@ structure_t = ["KO/TKO", "Submission", "Decision", "Finish score"]
 by_f = [{'label': s, 'value': s} for s in structure_f[2:]]
 by_r = [{'label': s, 'value': s} for s in structure_r[2:]]
 
-all_f_data = [fighter_data(all_fight_data, name) for name in all_f_names]
-all_r_data = [referee_data(all_fight_data, name) for name in all_r_names]
+load_figure_template("sketchy")
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+app = Dash(__name__, external_stylesheets=[dbc.themes.SKETCHY, dbc_css])
 server = app.server
 
 app.title = "Tomato"
-
-#A32116
 
 app.layout = dbc.Container(
     [
@@ -79,11 +82,11 @@ app.layout = dbc.Container(
                     ]
                 )
             ],
-            style={
-                "background-color": "#262B30",
-                "color": "white",
-                "padding": "20px",
-            },
+            # style={
+            #     "background-color": "#262B30",
+            #     "color": "white",
+            #     "padding": "20px",
+            # },
         ),
         # Body
         dbc.Container(
@@ -106,7 +109,7 @@ app.layout = dbc.Container(
                             id="column-1",
                             width=2,
                         ),
-                        dbc.Col(dcc.Graph(id="graph_line", config={"displayModeBar": False}), width=7),
+                        dbc.Col(dcc.Graph(id="graph_line", config={"displayModeBar": False}, className="container"), width=7),
 						dbc.Col(
 						    dash_table.DataTable(
 						        id="datatable1",
@@ -125,16 +128,18 @@ app.layout = dbc.Container(
 						        style_as_list_view=True,
 						    ),
 						    width=3,
-						)
+						    className="dbc dbc-row-selectable"
+						),
                     ]
                 ),
                 dbc.Row(id="row-2"),
             ],
-            style={"padding": "20px"},
+            # style={"padding": "20px"},
             fluid=True,
         ),
     ], 
     fluid=True,
+    className="dbc"
 )
 
 @app.callback(
@@ -147,7 +152,6 @@ def update_line_chart(group, names, value):
 		if 'All' in names:
 			names = all_f_names
 
-		all_n = all_f_names
 		all_data = all_f_data
 		structure = structure_f
 
@@ -155,25 +159,14 @@ def update_line_chart(group, names, value):
 		if "All" in names:
 			names = all_r_names
 
-		all_n = all_r_names
 		all_data = all_r_data
 		structure = structure_r
 
-	data = []
-	for name in names:
-		index = all_n.index(name)
-		for fight in all_data[index][1]:
-			data.append([name] + fight)
-	
-	if data != []:
-		df = pd.DataFrame(data)
-		df = df[[0, 1, structure.index(value)]]
-		df.columns = ["Name", "Date", value]
-	else:
-		df = pd.DataFrame(data, columns=["Name", "Date", value])
+	df= pd.DataFrame(all_data, columns=structure)
+	df = df[df['Name'].isin(names)]
 
 	fig = px.line(df, x="Date", y=value, color="Name")
-	fig.update_layout(showlegend=False, paper_bgcolor='#F7F7F7')
+	fig.update_layout(showlegend=False)
 	return fig
 
 @app.callback(
@@ -182,25 +175,21 @@ def update_line_chart(group, names, value):
     [Input("filter_graph_group", "value"),
 	Input("filter_graph_by", "value")])
 def update_datatable(group, value):
-
 	if group == "Fighters":
-		all_n = all_f_names
 		all_data = all_f_data
 		structure = structure_f
 
 	elif group == "Referees":
-		all_n = all_r_names
 		all_data = all_r_data
 		structure = structure_r
 
-	data = []
-	for i, name in enumerate(all_n):
-		data.append([name] + all_data[i][1][-1])
-	
-	df = pd.DataFrame(data)
-	df = df[[0, structure.index(value)]]
-	df = df.sort_values(structure.index(value))
-	df = df[::-1].reset_index(drop=True)
+	df= pd.DataFrame(all_data, columns=structure)
+	df = df.sort_values(by='Date', ascending=False)
+	df = df.drop_duplicates(subset='Name', keep='first')
+	df = df[["Name", value]]
+
+	# print(df)
+	df = df.sort_values(value, ascending=False).reset_index(drop=True)
 	df.index += 1
 	df = df.reset_index(level=0)
 	df.columns = ["#", "Name", value]
--- functions.py
+++ functions.py
@@ -38,39 +38,59 @@ def all_referee_names(data):
     return all_names 
 
 def fighter_data(data, name):
+    # strikes moet nog
+    # count = [winlose, ko, submission, decision, finish score, wins, losses, draws, knockdowns, 
+    #          takedowns landed, takedowns attempted, reversals, submission attempted, control time]
+    count = [0 for i in range(14)]
+    count[13] = datetime.strptime('00:00', '%M:%S').time()
     all_data = []
-    winlose = 0
-    # ko not implemented
-    ko = 0
-    decision = 0
-    submission = 0
-    finish = 0
     
     for event in data:
         for fight in event[0]:
             winner = fight[2][0]
 
-            if name in (fight[0][0], fight[1][0]):
+            fighters = [fight[0][0], fight[1][0]]
+            if name in fighters:
+                index = fighters.index(name)
+
+                # ["Knockdowns", "Takedowns Landed", "Takedowns Attempted", "Reversals", "Submission Attempted", "Control Time"]
+                if fight[index][1] != []:
+                    total = fight[index][1][0]
+
+                    count[8] += total[0]
+                    count[9] += total[1][0]
+                    count[10] += total[1][0]
+                    count[11] += total[2]
+                    count[12] += total[3]
+
+                    if total[4] != '--':
+                        time = datetime.strptime(total[4], '%M:%S').time()
+                    else:
+                        time = datetime.strptime('00:00', '%M:%S').time()
+
+                    count[13]= datetime.combine(datetime.now(), time)
+
                 if fight[winner][0] == name:
-                    winlose += 1
+                    count[0] += 1
+                    count[5] += 1
 
                     if fight[2][1] == 'KO/TKO':
-                        ko += 1
-                        finish += 1
+                        count[1] += 1
+                        count[4] += 1
                     elif fight[2][1] == 'Submission':
-                        submission += 1
-                        finish += 1
+                        count[2] += 1
+                        count[4] += 1
                     else:
-                        decision += 1
-                        finish -= 1
+                        count[3] += 1
+                        count[4] -= 1
 
                 elif winner == 2:
-                    continue
+                    count[7] += 1
                 else:
-                    winlose -= 1
+                    count[0] -= 1
+                    count[6] += 1
             
-                date = convert_date(event[1])
-                data = [date, ko, submission, decision, winlose, finish]
+                data = [convert_date(event[1])] + count
                 all_data.append(data)
     
     return [name, all_data]
